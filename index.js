a78600808ad626136ee35743622d3f39c0279036
Brajesh Kumar <brajeshkumaryadavjio2@gmail.com>
2024-08-23 20:48:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// You can add interactivity here, for example, a smooth scroll for navigation links
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
